e0dde6be7dce120b3a236b56c7db671ea21c9157
ahmadli-codex <ibrahim.ahmadli06@outlook.com>
2023-03-25 22:17:12 -0500

    1

--- src/Views/LandingPage/index.jsx
+++ src/Views/LandingPage/index.jsx
@@ -4,7 +4,6 @@ const LandingPage = () => {
     return (
         <div>
             <p>Click this link to get more views!</p>
-            <img src={process.env.REACT_APP_PUBLIC_URL}></img> 
             <button>Watch Free Video!</button>
         </div>
     )
